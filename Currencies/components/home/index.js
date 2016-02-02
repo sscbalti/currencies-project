bc72825024a80bb45e265435c04d0bf5a4240dcf
Baltisar Oswald <baltisar.oswald@gmail.com>
2016-02-02 21:34:53 +0000

    Changes from Views service

--- Currencies/components/home/index.js
+++ Currencies/components/home/index.js
@@ -9,3 +9,43 @@ app.home = kendo.observable({
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_home
+(function(parent) {
+    var dataProvider = app.data.currenciesDataProvider,
+        dataSourceOptions = {
+            type: 'json',
+            transport: {
+                read: {
+                    url: dataProvider.url
+                }
+            },
+
+            schema: {
+                data: '',
+                model: {
+                    fields: {
+                        'source': {
+                            field: 'source',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        homeModel = kendo.observable({
+            dataSource: dataSource
+        });
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('homeModel', homeModel);
+        });
+    } else {
+        parent.set('homeModel', homeModel);
+    }
+})(app.home);
+
+// START_CUSTOM_CODE_homeModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_homeModel
---
+++ Currencies/dataProviders/currenciesDataProvider.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function() {
+    app.data.currenciesDataProvider = {
+        url: 'http://www.apilayer.net/api/historical?access_key=940541b46b4603ca3e5d5640bc110257&date=2016-02-02&currencies=USD,CHF,EUR'
+    }
+}());
+
+// START_CUSTOM_CODE_currenciesDataProvider
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_currenciesDataProvider
