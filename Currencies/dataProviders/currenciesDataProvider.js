22c2ce85eaaa787bd190e20f34d1e415704099df
Benjamin Haegler <benjaminhaegler@gmail.com>
2016-02-02 21:44:01 +0000

    Changes from Views service

--- Currencies/dataProviders/currenciesDataProvider.js
+++ Currencies/dataProviders/currenciesDataProvider.js
@@ -2,7 +2,7 @@
 
 (function() {
     app.data.currenciesDataProvider = {
-        url: 'http://www.apilayer.net/api/historical?access_key=940541b46b4603ca3e5d5640bc110257&date=2016-02-02&currencies=USD,CHF,EUR'
+        url: 'https://www.apilayer.net/api/historical?access_key=940541b46b4603ca3e5d5640bc110257&date=2016-02-02&currencies=USD,CHF,EUR'
     }
 }());
 
